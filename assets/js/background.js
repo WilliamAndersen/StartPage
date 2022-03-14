14a66fee5f5d97204cf0a0ee053873aa2eab5997
WilliamAndersen <william_andersen@live.com>
2022-03-14 13:15:16 +0100

    Random background picker

---
+++ assets/js/background.js
@@ -0,0 +1,9 @@
+var random_images_array = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+
+function getRandomImage(imgAr, path) {
+    path = path || 'assets/background'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "">';
+    document.write(imgStr); document.close();
+}
